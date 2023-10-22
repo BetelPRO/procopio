bf5dcea5c0b199fb542fc0494b40cb9a82664802
Deivid Procópio <deividprocopio7@gmail.com>
2023-10-22 16:49:34 -0300

    atual

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,17 @@
 function menu() {
     const menu = document.getElementById('menu');
     const exit =document.getElementById('exit');
+    const deitado = document.getElementById('deitado');
+    deitado.classList.add('deitado')
     menu.classList.add("menuAtivo");
-    exit.classList.add('Off')
+    exit.classList.add('Off');
 };
 
 function exit() {
-    const exit = document.getElementById('exit')
-    const menu = document.getElementById('menu')
-    menu.classList.remove('menuAtivo')
-    exit.classList.remove('Off')
+    const exit = document.getElementById('exit');
+    const menu = document.getElementById('menu');
+    const deitado = document.getElementById('deitado');
+    deitado.classList.remove('deitado')
+    menu.classList.remove('menuAtivo');
+    exit.classList.remove('Off');
 }
