7458d95175ebce1b0f6650fbcdb91d2c5326a408
Deivid Procópio <deividprocopio7@gmail.com>
2023-10-23 19:26:12 -0300

    atual

---
+++ js/pedidos.js
@@ -0,0 +1,17 @@
+function select4() {
+    const quatro = document.getElementById("_400ml")
+
+    quatro.classList.add("_400m")
+    const treze = document.getElementById("_300ml")
+
+    treze.classList.remove("_300m")
+}
+
+function select3() {
+    const treze = document.getElementById("_300ml")
+
+    treze.classList.add("_300m")
+    const quatro = document.getElementById("_400ml")
+
+    quatro.classList.remove("_400m")
+}
