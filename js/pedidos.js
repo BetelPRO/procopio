05ab6e0291a1537ed1047e68aa1d77434ec21d30
Deivid Procópio <deividprocopio7@gmail.com>
2023-10-25 03:53:12 -0300

    atual

--- js/pedidos.js
+++ js/pedidos.js
@@ -15,3 +15,86 @@ function select3() {
 
     quatro.classList.remove("_400m")
 }
+
+function one() {
+    if (document.getElementById("leite").checked) {
+        const leite = document.getElementById("leite_jpg");
+        leite.classList.toggle("peg");
+    };
+    
+    if (document.getElementById("paçoca").checked) {
+        const paçoca = document.getElementById("paçoca_jpg")
+        paçoca.classList.toggle("peg")
+    };
+}
+
+function two() {
+    if (document.getElementById("granola").checked) {
+        const granola = document.getElementById("granola_jpg");
+        granola.classList.toggle("peg");
+    };
+    
+    if (document.getElementById("jujuba").checked) {
+        const jujuba = document.getElementById("jujuba_jpg")
+        jujuba.classList.toggle("peg")
+    };
+}
+
+function tree() {
+    if (document.getElementById("confete").checked) {
+        const confete = document.getElementById("confete_jpg");
+        confete.classList.toggle("peg");
+    };
+    
+    if (document.getElementById("bolinha").checked) {
+        const bolinha = document.getElementById("bolinha_jpg")
+        bolinha.classList.toggle("peg")
+    };
+}
+
+function con() {
+    ingredientes = [];
+
+    if (document.getElementById("_400").checked) {
+        pedido = "400 ml \n";
+    };
+    
+    if (document.getElementById("_300").checked) {
+        pedido = "300 ml \n";
+    };
+
+    if(document.getElementById("leite").checked) {
+        pedido = pedido + "Leite \n";
+    };
+    
+    if(document.getElementById("paçoca").checked) {
+        pedido = pedido + "Paçoca \n";
+    };
+    
+    if(document.getElementById("granola").checked) {
+        pedido = pedido + "Granola \n";
+    };
+    
+    if(document.getElementById("jujuba").checked) {
+        pedido = pedido + "Jujuba \n";
+    };
+    
+    if(document.getElementById("confete").checked) {
+        pedido = pedido + "Confete\n";
+    };
+    
+    if(document.getElementById("bolinha").checked) {
+        pedido = pedido + "Bolinha\n";
+    };
+
+    if (document.getElementById("_400").checked || document.getElementById("_300").checked){
+        let mensagem;
+        mensagem = "Olá gostaria de pedir um Açaí de " + pedido;
+        alert(mensagem);
+        window.open("https://wa.me/+5581986728498?text=" + mensagem);
+    }
+    else {
+        alert("Escolha o Tamanho do Açaí")
+    }
+};
+
